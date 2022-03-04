ed26891f2401be6c93d87160a27f6a1c168f829e
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-04 01:13:40 -0500

    uploaded images into Asset folder and linked them to index.css and slider.js

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 
+
 export default class Slider extends Component {
   render() {
     return (
@@ -17,28 +18,28 @@ export default class Slider extends Component {
             <div class="slider">
             <ul class="slides">
                 <li>
-                    <img src="https://lorempixel.com/580/250/nature/1">
+                    <img className='Forest'/>
                     <div class="caption center-align">
                       <h3>This is our big Tagline!</h3>
                       <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                     </div>
                 </li>
                 <li>
-                    <img src="https://lorempixel.com/580/250/nature/2"> 
+                    <img className='Birds'/> 
                     <div class="caption left-align">
                         <h3>Left Aligned Caption</h3>
                         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                     </div>
                 </li>
                 <li>
-                    <img src="https://lorempixel.com/580/250/nature/3"> 
+                    <img className='Neighbor'/> 
                     <div class="caption right-align">
                         <h3>Right Aligned Caption</h3>
                         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                     </div>
                 </li>
                 <li>
-                    <img src="https://lorempixel.com/580/250/nature/4"> 
+                    <img className='Sunset'/> 
                     <div class="caption center-align">
                         <h3>This is our big Tagline!</h3>
                         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
