ebec5824e18d85c7831a209c4bce3aae1fadf4fd
Caitlin Wall <caitlinwall96@gmail.com>
2022-03-04 11:21:44 -0500

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  profile: {
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
