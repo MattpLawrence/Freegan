fa64387c1deb7db9c3e283fec11ceea66e6689b4
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-04 10:36:39 -0500

    add socket.io dep

--- server/server.js
+++ server/server.js
@@ -1,28 +1,27 @@
-const express = require ('express');
-const {ApolloServer} = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const {typeDefs, resolvers} = require('./schemas');
-const db = require ('./config/connection');
-const { type } = require('os');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { type } = require("os");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
-
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Add build lines later
 
 // Start server
 const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start()
-    console.log(`Server is running ${PORT}`)
+  await server.start();
+  console.log(`Server is running ${PORT}`);
 };
 
 // Call function to start the server
