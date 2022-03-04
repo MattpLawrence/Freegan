049e2a058b53c837be23a4a040a92ea8581a804a
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-03 23:37:36 -0500

    df

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,11 +4,18 @@ import {  Link } from "react-router-dom";
 
 const navbar = () => {
     return (
-        <div>
-            <a href="#" class="brand-logo">Logo</a>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Freegan</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><Link to="/login">Log-In</Link></li>
             </ul>
+            <form>
+        <div class="input-field">
+          <input id="search" type="search" required></input>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
         </div>
     );
 }
