41bcd409133459e375214c6edbf52d8b2059fe34
Caitlin Wall <caitlinwall96@gmail.com>
2022-03-04 10:56:07 -0500

    Add imports for apollo/client

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import '../src/components/Slider'
+import React from "react";
+import "../src/components/Slider";
+import { setContext } from "@apollo/client/link/context";
+import { createHttpLink } from "@apollo/client";
 
-import Home from './pages/Home';
-import Member from './pages/Member';
+import Home from "./pages/Home";
+import Member from "./pages/Member";
 
 // constructor that will be used for GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
-
 function App() {
-  return (
-    <div className='App'>
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
