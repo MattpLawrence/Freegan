c049e2915a9c4f184a2d53c53571883be6dc8fe5
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-04 13:52:16 -0500

    created Query for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,12 +22,35 @@ const resolvers = {
         item: async (parent, { id }) => 
             Item.findById(id).populate('category'),
         
-        user: async (parent, args, context) =>
+        user: async (parent, args, context) => {
             if (context.user) {
-                const user = await User.findById
+                const user = await User.findById(context.user.id).populate({
+                    path:'orders.items',
+                    populate: 'category',
+                });
+                
+                return user;
             }
-            
+            throw new AuthenticationError('Not logged in');
+        },
 
-    },
-}
+        order: async (parent, { id }, context) => {
+            if (context.user) {
+                const user = await User.findById(context.   user.id).populate({
+                    path: 'orders.items',
+                    populate: 'category',
+        });
 
+        return user.orders.id(id);
+      }
+
+      throw new AuthenticationError('Not logged in');
+        },
+    },  
+
+    Mutation: {
+        
+    }
+};
+
+module.exports = resolvers;
