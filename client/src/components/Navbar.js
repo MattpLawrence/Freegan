4b6efd215c6afdaae3b55ce6876d72cf0447c215
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-04 14:00:54 -0500

    removed searchbar from navbar component and created separate searchbad component page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,13 +9,6 @@ const navbar = () => {
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><Link to="/login">Log-In</Link></li>
             </ul>
-            <form>
-        <div class="input-field">
-          <input id="search" type="search" required></input>
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-          <i class="material-icons">close</i>
-        </div>
-      </form>
         </div>
     );
 }
