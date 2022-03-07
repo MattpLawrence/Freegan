8028fd11654869b510a32116abc42cdc25d851c7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-07 09:26:57 -0500

    prettier

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React from "react";
 import Auth from "../../utils/auth";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function navbar() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <div className="nav-wrapper">
           <ul id="nav-mobile" className="right hide-on-med-and-down flex-row">
             <li className="mx-1 brand-logo">
-              <Link to="/">
-                Freegan
-              </Link>
+              <Link to="/">Freegan</Link>
             </li>
             <li className="mx-1">
-              <Link to="/dashboard">
-                Dashboard
-              </Link>
+              <Link to="/dashboard">Dashboard</Link>
             </li>
             <li className="mx-1">
-              <Link to="/profile">
-                Profile
-              </Link>
+              <Link to="/profile">Profile</Link>
             </li>
             <li className="mx-1">
               <a href="/" onClick={() => Auth.logout()}>
@@ -31,26 +24,19 @@ function navbar() {
             </li>
           </ul>
         </div>
-        
       );
     } else {
       return (
         <div className="nav-wrapper">
           <ul id="nav-mobile" className="right hide-on-med-and-down flex-row">
-          <li className="mx-1 brand-logo">
-              <Link to="/">
-                Freegan
-              </Link>
+            <li className="mx-1 brand-logo">
+              <Link to="/">Freegan</Link>
             </li>
             <li className="mx-1">
-              <Link to="/signup">
-                Signup
-              </Link>
+              <Link to="/signup">Signup</Link>
             </li>
             <li className="mx-1">
-              <Link to="/login">
-                Log-In
-              </Link>
+              <Link to="/login">Log-In</Link>
             </li>
           </ul>
         </div>
@@ -60,9 +46,7 @@ function navbar() {
 
   return (
     <header className="flex-row px-1">
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,55 +1,57 @@
-import React, { Component } from 'react'
-import 'materialize-css/dist/css/materialize.min.css'
-
+import React, { Component } from "react";
+import "materialize-css/dist/css/materialize.min.css";
 
 export default class Slider extends Component {
   render() {
     return (
-      <div className='container'>
-            <div className="row">
-              <div className='col s6'>
-                Col 1
-              </div>
-              <div className='col s6'>
-                  Col 2
-              </div>
-            </div>
-            <div className='container'>
-            <div class="slider">
+      <div className="container">
+        <div className="row">
+          <div className="col s6">Col 1</div>
+          <div className="col s6">Col 2</div>
+        </div>
+        <div className="container">
+          <div class="slider">
             <ul class="slides">
-                <li>
-                    <img className='Forest'/>
-                    <div class="caption center-align">
-                      <h3>This is our big Tagline!</h3>
-                      <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                    </div>
-                </li>
-                <li>
-                    <img className='Birds'/> 
-                    <div class="caption left-align">
-                        <h3>Left Aligned Caption</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                    </div>
-                </li>
-                <li>
-                    <img className='Neighbor'/> 
-                    <div class="caption right-align">
-                        <h3>Right Aligned Caption</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                    </div>
-                </li>
-                <li>
-                    <img className='Sunset'/> 
-                    <div class="caption center-align">
-                        <h3>This is our big Tagline!</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                    </div>
-                </li>
+              <li>
+                <img className="Forest" />
+                <div class="caption center-align">
+                  <h3>This is our big Tagline!</h3>
+                  <h5 class="light grey-text text-lighten-3">
+                    Here's our small slogan.
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img className="Birds" />
+                <div class="caption left-align">
+                  <h3>Left Aligned Caption</h3>
+                  <h5 class="light grey-text text-lighten-3">
+                    Here's our small slogan.
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img className="Neighbor" />
+                <div class="caption right-align">
+                  <h3>Right Aligned Caption</h3>
+                  <h5 class="light grey-text text-lighten-3">
+                    Here's our small slogan.
+                  </h5>
+                </div>
+              </li>
+              <li>
+                <img className="Sunset" />
+                <div class="caption center-align">
+                  <h3>This is our big Tagline!</h3>
+                  <h5 class="light grey-text text-lighten-3">
+                    Here's our small slogan.
+                  </h5>
+                </div>
+              </li>
             </ul>
-</div>
-              
-            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
