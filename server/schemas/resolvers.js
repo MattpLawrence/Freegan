32cade46fe838b7e8403ec9a0a36f010bf073d1f
Rachel Elizabeth Washington <rwashington325@gmail.com>
2022-03-06 22:04:05 -0700

    completes typeDefs file

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { Category, Item, Profile, User } = require('../models');
+
+const resolvers = {
+  Query: {
+    items: async ()=> {
+
+    },
+    profiles: async () => {
+
+    },
+    users: async () => {
+    },
+    categories: async () => {
+
+    },
+  }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Item {
+    _id: ID
+    name: String
+    description: String
+    quantity: Int
+    category: [Category]
+  }
+
+  type Profile {
+    _id: ID
+    firstName: String
+    lastName: String
+    zipCode: Int
+    photo: String
+    profile: [Profile]
+  }
+
+  type User {
+    _id: ID
+    email: String
+    password: String
+    profile: [Profile]
+    items: [Item]
+  }
+
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Query {
+    items: [Item]
+    profiles: [Profile]
+    users: [User]
+    categories:[Category]
+  }
+`;
+
+module.exports = typeDefs;
