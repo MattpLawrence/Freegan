90f7741be620b14c825563aa5d8f78ba01120a02
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-07 14:08:16 -0500

    adding states to register page

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/signup.css";
 
 function SignUp() {
+  const [userInfo, setUserInfo] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleChange(e) {
+    if (e.target.id === "firstName") {
+      setFirstName(e.target.value);
+      console.log(firstName);
+    } else if (e.target.id === "lastName") {
+      setLastName(e.target.value);
+      console.log(lastName);
+    } else if (e.target.id === "email") {
+      setEmail(e.target.value);
+      console.log(email);
+    } else {
+      setPassword(e.target.value);
+      console.log(password);
+    }
+  }
+
   return (
     <div>
       <div className="container signUpContainer">
@@ -10,26 +32,37 @@ function SignUp() {
             <div className="row">
               <div className="input-field col s6">
                 <input
-                  id="first_name"
+                  id="firstName"
                   type="text"
                   className="validate"
+                  value={firstName}
+                  onChange={handleChange}
                   required
                 />
                 <label htmlFor="first_name">First Name</label>
               </div>
               <div className="input-field col s6">
                 <input
-                  id="last_name"
+                  id="lastName"
                   type="text"
                   className="validate"
+                  value={lastName}
                   required
+                  onChange={handleChange}
                 />
                 <label htmlFor="last_name">Last Name</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input id="email" type="email" className="validate" required />
+                <input
+                  id="email"
+                  type="email"
+                  className="validate"
+                  required
+                  value={email}
+                  onChange={handleChange}
+                />
                 <label htmlFor="email">Email</label>
               </div>
             </div>
@@ -41,6 +74,8 @@ function SignUp() {
                   className="validate"
                   minLength="6"
                   required
+                  value={password}
+                  onChange={handleChange}
                 />
                 <label htmlFor="password">Password</label>
               </div>
