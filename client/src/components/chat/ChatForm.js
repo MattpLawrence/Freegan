c1cd5e85dfd51ccb259f649ef3a3713837667aa5
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-07 08:54:30 -0500

    update chatform to add message function without username

--- client/src/components/chat/ChatForm.js
+++ client/src/components/chat/ChatForm.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-function ChatForm() {
+function ChatForm(props) {
+  const [msg, setMsg] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    (() => {
+      props.addMessage({
+        id: Math.random(Math.floor(Math.random() * 8)),
+        msgText: msg,
+        userName: "tbd",
+      });
+      setMsg("");
+    })();
+  };
+
   return (
     <form id="chat-form">
       <input
         id="msg"
         type="text"
         placeholder="Enter Message"
+        value={msg}
         required
         autocomplete="off"
       />
