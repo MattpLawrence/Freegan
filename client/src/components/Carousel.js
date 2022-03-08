52cf8cecccf870b55d0bc8fce73fee53baac2eb7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-03-08 12:32:01 -0500

    add item component

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -10,11 +10,8 @@ import five from "./assets/images/sunset.jpg";
 class Carousel extends Component {
   componentDidMount() {
     const options = {
-      fullWidth: true,
       duration: 300,
-      onCycleTo: () => {
-        console.log("New Slide");
-      },
+      onCycleTo: () => {},
     };
     M.Carousel.init(this.Carousel, options);
 
@@ -31,21 +28,22 @@ class Carousel extends Component {
         }}
         className="carousel "
       >
-        <a className="carousel-item">
-          <img alt="1" src={one} />
-        </a>
-        <a className="carousel-item">
-          <img alt="2" src={two} />
-        </a>
-        <a className="carousel-item">
-          <img alt="3" src={three} />
-        </a>
-        <a className="carousel-item">
-          <img alt="4" src={four} />
-        </a>
-        <a className="carousel-item">
-          <img alt="5" src={five} />
-        </a>
+        <div class="carousel-item red white-text" href="#one!">
+          <h2>First Panel</h2>
+          <p class="white-text">This is your first panel</p>
+        </div>
+        <div class="carousel-item amber white-text" href="#two!">
+          <h2>Second Panel</h2>
+          <p class="white-text">This is your second panel</p>
+        </div>
+        <div class="carousel-item green white-text" href="#three!">
+          <h2>Third Panel</h2>
+          <p class="white-text">This is your third panel</p>
+        </div>
+        <div class="carousel-item blue white-text" href="#four!">
+          <h2>Fourth Panel</h2>
+          <p class="white-text">This is your fourth panel</p>
+        </div>
       </div>
     );
   }
